996c9de8ddbd4f7d34f41aa9573544001bce3bd5
Phil Flournoy <phil@itgembedded.com>
2017-04-03 18:04:25 -0400

    Minor Adjustments

--- include/configs/Q6.h
+++ include/configs/Q6.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 ITG
+ * Copyright (C) 2012 Freescale Semiconductor, Inc.
  *
  * Configuration settings for the JCI Q6 board.
  *
@@ -15,25 +15,33 @@
 #include "imx6_spl.h"
 #endif
 
-#define CONFIG_MACH_TYPE	3980
+#define MACH_TYPE_Q6		5082	
+#define CONFIG_MACH_TYPE	MACH_TYPE_Q6
 #define CONFIG_MXC_UART_BASE	UART1_BASE
-#define CONFIG_CONSOLE_DEV		"ttymxc0"
-#define CONFIG_MMCROOT			"/dev/mmcblk2p2"
-#define PHYS_SDRAM_SIZE		(1u * 1024 * 1024 * 1024)
+#define CONFIG_CONSOLE_DEV	"ttymxc0"
+#define CONFIG_MMCROOT		"/dev/mmcblk1p2"
+#define CONFIG_SATAROOT		"/dev/sda2"
 
-#define CONFIG_SUPPORT_EMMC_BOOT /* eMMC specific */
+/* PARAMETERS FROM _defconfig (they are called there without CONFIG_)*/
+#define CONFIG_OPENREX_DEFAULT_ARCH_PREFIX  CONFIG_BOARD_TYPE_PREFIX
+#define CONFIG_OPENREX_DEFAULT_ARCH_POSTFIX  CONFIG_BOARD_TYPE_POSTFIX
+
+#if defined(CONFIG_DDR_SIZE) 
+#define PHYS_SDRAM_SIZE         CONFIG_DDR_SIZE
+#else
+#define PHYS_SDRAM_SIZE         SZ_2G
+#warning "Using default SDRAM size"
+#endif
 
 #define VIDEO_ARGS        "${video_args}"
 #define VIDEO_ARGS_SCRIPT "run video_args_script; "
 
+/* This include must follow VIDEO_ARGS defs, otherwise re-defined */
 #include "Q6_common.h"
 
-#define CONFIG_SYS_FSL_USDHC_NUM	3
-#if defined(CONFIG_ENV_IS_IN_MMC)
-#define CONFIG_SYS_MMC_ENV_DEV		1	/* SDHC3 */
-#endif
+#define CONFIG_SYS_FSL_USDHC_NUM	1
 
-#define CONFIG_CMD_PCI
+#undef CONFIG_CMD_PCI
 #ifdef CONFIG_CMD_PCI
 #define CONFIG_PCI
 #define CONFIG_PCI_PNP
@@ -53,12 +61,12 @@
 #define CONFIG_SYS_I2C_SPEED		  100000
 
 /* PMIC */
-/*
+#if 0
 #define CONFIG_POWER
 #define CONFIG_POWER_I2C
 #define CONFIG_POWER_PFUZE100
 #define CONFIG_POWER_PFUZE100_I2C_ADDR	0x08
-*/
+#endif
 
 /* USB Configs */
 #define CONFIG_CMD_USB
@@ -71,7 +79,23 @@
 #define CONFIG_USB_ETHER_ASIX
 #define CONFIG_MXC_USB_PORTSC		(PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CONFIG_MXC_USB_FLAGS		0
-#define CONFIG_USB_MAX_CONTROLLER_COUNT	1 /* Enabled USB controller number */
+#define CONFIG_USB_MAX_CONTROLLER_COUNT	2 /* Enabled USB controller number */
+#endif
+
+#ifdef CONFIG_MX6Q
+#define CONFIG_CMD_SATA
+#endif
+
+/*
+ * SATA Configs
+ */
+#ifdef CONFIG_CMD_SATA
+#define CONFIG_DWC_AHSATA
+#define CONFIG_SYS_SATA_MAX_DEVICE	1
+#define CONFIG_DWC_AHSATA_PORT_ID	0
+#define CONFIG_DWC_AHSATA_BASE_ADDR	SATA_ARB_BASE_ADDR
+#define CONFIG_LBA48
+#define CONFIG_LIBATA
 #endif
 
 #endif                         /* __Q6_CONFIG_H */
